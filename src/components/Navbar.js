2104153e8dc873430edf9502353d2072089441b4
PrabaDT <devz.hettiarachchi@gmail.com>
2024-09-30 01:13:10 +0530

    change nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,8 @@ function Navbar() {
   return (
     <div className='flex items-center w-full h-24 mx-auto border-b-[1px] border-b-gray-600 gap-x-80 px-8 md:px-16'>
         
-        <div className='flex'>
-          <text>PRABA</text>
-        </div>
 
-        <div className='flex justify-center gap-20'>
-          <a href='banner'>Home</a>
-          <a href='About'>About me</a>
-          <a href='Projects'>Projects</a>
-          <a href='Resume'>Contact me</a>
-        </div>
+
 
     </div>
   )
